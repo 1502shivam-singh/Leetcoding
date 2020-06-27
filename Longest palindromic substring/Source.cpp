18652adabdd6b2e4603ed5b0f2ebce37e1f4f4a3
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-06-27 19:19:17 +0530

    Added code to source file

--- Longest palindromic substring/Source.cpp
+++ Longest palindromic substring/Source.cpp
@@ -1,1 +1,33 @@
+//All necessary headers considered included as of Leetcode platform
 
+class Solution {
+public:
+    string longestPalindrome(const string& s);  //Solution function
+};
+
+string Solution::longestPalindrome(const string& s){
+    int n = s.size(), ptr1=0, ptr2=0, flag=0;
+	int k = 0, l = 0, e = 0, len = 0;
+		for (int i = 0; i < n; i++) {
+			e = s.size() - 1;
+            flag=e;
+			k = i; l = e;
+			while (k <= e) {
+				if (s[k] == s[e]) { k++, e--; }
+				else {
+					if (k >= i) {
+						k = i;
+						flag--;
+						l = flag;
+						e = flag;
+					}
+				}
+			}
+				if((l-i+1)>=len){
+					ptr1 = i;
+					ptr2 = l;
+					len = l - i + 1;
+				}
+	}
+	return s.substr(ptr1, (ptr2 - ptr1 + 1));      
+}
