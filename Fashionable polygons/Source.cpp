07bad9c9c1dade41d23ab9bd03a2d3fea73d2837
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-06-28 23:27:55 +0530

    Added code to cpp

--- Fashionable polygons/Source.cpp
+++ Fashionable polygons/Source.cpp
@@ -1,1 +1,18 @@
-
+    #include<iostream>
+    #include<cmath>
+    #include<vector>
+     
+    int main() {
+    	int num;
+    	double val, ref;
+    	std::vector<double> arr;
+    	std::cin >> num;	//Data input
+    	for (int i = 0; i<num; i++) {
+    		std::cin >> val;
+    		arr.push_back(val);	//Data input
+    	}
+    	for (int i = 0; i<num; i++) {
+    		if (std::modf((arr[i]-4)/4, &ref) == 0.0) { std::cout << "YES\n"; }	//Solution
+    		else { std::cout << "NO\n"; }
+    	}
+    }
