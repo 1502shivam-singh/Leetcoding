a7422ed83c7240e6e99f2b835098507b5af1be97
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-07-03 22:43:49 +0530

    Added code

--- Non-decreasing array/Source.cpp
+++ Non-decreasing array/Source.cpp
@@ -1,1 +1,27 @@
 //All header files considered added as on the Leetcode platform
+class Solution {
+private:
+    int check=0;
+public:
+    bool checkPossibility(vector<int>& nums);
+};
+
+bool Solution::checkPossibility(vector<int>& nums) {
+        for(int i=0;i<nums.size()-1;i++){
+            if(!(nums[i]<=nums[i+1])){
+                if(i>0){
+                    if(nums[i+1]>=nums[i-1]){
+                        nums[i]=nums[i-1];
+                        check++;
+                    }
+                    else{
+                        nums[i+1]=nums[i];
+                        check++;
+                    }
+                }
+                else{nums[i]=nums[i+1];check++;}
+            }
+        }
+  if(check>1){return false;}
+  else{return true;}
+}
