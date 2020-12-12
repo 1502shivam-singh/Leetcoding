1031c3413e91674cac0d173378d30ee3d42e8cc9
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-12-12 06:58:30 +0530

    Added pseudocode for a different approach solution
    This solution uses dynamic programming at its core for getting the solution

--- Longest palindromic substring/Source.cpp
+++ Longest palindromic substring/Source.cpp
@@ -31,3 +31,56 @@ string Solution::longestPalindrome(const string& s){
 	}
 	return s.substr(ptr1, (ptr2 - ptr1 + 1));      
 }
+
+/*
+Another neater solution using dynamic programming
+
+Divide into smaller strings.
+
+Reach smallest string, build up now forming a palindrome (if formed)
+
+Map the substring (a std::pair<int,int> for start and end index) to 0 (if not palindrome) or 1 (if palindrome)
+
+Check if the length is greater, if true replace it using the preexisting palindromes in the table 
+
+bab + bcb = X   (Unequal palindromes won't make a palindrome)
+bab + bab = 0   (Equal palindromes will make a palindrome)
+So, O(n) check for comparing string
+
+palindrome + not palidrome = X 
+non palindrom + not palindrome = 0 or X
+
+// Pseudo code
+std::unordered_map<std::pair<int,int>, bool> hash;
+int start = 0, end = 0
+void functionLP(str, s, e)
+{
+    if(s != e)
+    {
+        functionLP(str, 0, s);
+        functionLP(str, s+1, e);
+    }
+    else 
+    {
+        hash[std::pair<int,int>(s,s)] = 1;
+    }
+    if(hash[<0,s>]==0 && hash[<s+1,e>]==0)
+    {
+        // Check for palindrome in s,e
+        // if yes then reassign start and end, after comparing current length
+        // else let them stay unchanged, and mark this pair in hash as 0
+    }
+    else if(hash[<0,s>]==1 && hash[<s+1,e>]==0) // Or its opposite
+    {
+        hash[<s,e>] = 0;
+    }
+    else
+    {
+        // check both strings for equality (<0,s> <s,e>), 
+        // mark hash 1 for this if equal and reassign strt and end (after checking length)
+        // else let them stay unchanged, and mark this pair in hash as 0. 
+    }   
+}
+
+call functionLP in longestPalindrome
+*/
