f7ba0640aa905aa9583e29bc272ca2025f47f0a5
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2021-01-27 07:10:28 +0530

    Added comments for better understanding

--- Remove Nth node from end/createGapApproachOfN.cpp
+++ Remove Nth node from end/createGapApproachOfN.cpp
@@ -8,6 +8,11 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//  make distace between headPtr and tailPtr equal to n (in case in creating this distance the pointer nulls, iterate till node is not nullptr)
+//  if headPtr is not null, move both tail and head untill head is null. When head is at null, you are the nth pos from end
+//   when headPtr->next is null make, tailPtr->next = tailPtr->next->next;
+
 class Solution {
     private:
         int place = 0;
